0fe198270473fc45ffbe0b26061a6aaa322f4c71
NomanSadick <nomansadickjs@gmail.com>
2020-12-24 06:54:14 +0600

    done

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
-
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json())
+
+const users = ['samir', 'foisal', 'babu', 'tomal', 'tuhin', 'abhi', 'noyon'];
+
 app.get('/', (req, res) =>{
-    res.send('Thank you very much');
+    const fruts = {
+        product : 'banana',
+        price :230
+    }
+    res.send(fruts);
+})
+
+
+app.get('/users/:id', (req, res) =>{
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id, name})
+})
+
+//post
+app.post('/addUser', (req, res) =>{
+    //save to database
+    const user = req.body;
+    user.id = 5;
+    res.send(user);
 })
 
 app.listen(3000, () => console.log('Lisening to port 3000'));
